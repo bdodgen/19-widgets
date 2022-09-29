ac6340524592a97f2cae511ff156c92ce1cee129
Briana Dodgen <brianadodgen@gmail.com>
2022-09-29 15:49:34 +0200

    completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 import { ProgressBar } from "./ProgressBar";
 
-const body = document.querySelector('body');
+const progBarDiv = document.querySelector('.progress-bars');
 const redProgBar = new ProgressBar(0, 1, 17, 255, 0, 0);
 const greenProgBar = new ProgressBar(0, 1, 17, 0, 255, 0);
 const blueProgBar = new ProgressBar(0, 1, 17, 0, 0, 255);
+const colorizeBtn = document.querySelector('.colorize-button')
 
-body.appendChild(redProgBar.element);
-body.appendChild(greenProgBar.element);
-body.appendChild(blueProgBar.element);
+progBarDiv.appendChild(redProgBar.element);
+progBarDiv.appendChild(greenProgBar.element);
+progBarDiv.appendChild(blueProgBar.element);
+
+colorizeBtn.addEventListener('click', () => {
+  const redVal = redProgBar.value * 15;
+  const greenVal = greenProgBar.value * 15;
+  const blueVal = blueProgBar.value * 15;
+  const calculatedColor = `rgb(${redVal}, ${greenVal}, ${blueVal})`;
+  colorizeBtn.style.backgroundColor = calculatedColor;
+
+})
+
